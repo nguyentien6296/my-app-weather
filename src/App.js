d04b41e6b5b782a031bcbf3760ec9e79511c256f
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2022-12-29 10:11:41 +0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="http://nguyencaroline.com/" target="_blank">
-          Caroline Nguyen
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/nguyentien6296/my-app-weather"
-          rel="noopener"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="http://nguyencaroline.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Caroline Nguyen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nguyentien6296/my-app-weather"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
