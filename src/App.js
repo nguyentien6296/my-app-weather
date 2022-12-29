2dd5112b09404f710e19f2671ea71e0906ce4ec3
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2022-12-29 08:29:18 +0700

    Added the Github Res url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="http://nguyencaroline.com/" target="_blank">
+          Caroline Nguyen
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/nguyentien6296/my-app-weather"
+          rel="noopener"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
