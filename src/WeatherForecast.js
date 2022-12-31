e8c5b3c8e63b1882cdecb634bbe6af0b7aecb2e3
nguyentien6296 <88523384+nguyentien6296@users.noreply.github.com>
2022-12-31 14:29:46 +0700

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,11 +4,11 @@ import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
+  let [loaded, setLoaded] = useState(true);
   let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
     setForecast(response.data.daily);
-    setLoaded(true);
+    setLoaded(false);
   }
   if (loaded) {
     return (
@@ -31,7 +31,6 @@ export default function WeatherForecast(props) {
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&long=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-
     return null;
   }
 }
